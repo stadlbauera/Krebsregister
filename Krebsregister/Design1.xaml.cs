c6604768dc559c822c884f14bb9d0d8a64c8652b
stadlbauera <StadlbauerA180120@sus.htl-grieskirchen.at>
2022-10-19 15:50:20 +0200

    Charts und Designs vom 19.10.22

---
+++ Krebsregister/Design1.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Krebsregister
+{
+    /// <summary>
+    /// Interaktionslogik für Design1.xaml
+    /// </summary>
+    public partial class Design1 : Window
+    {
+        public Design1()
+        {
+            InitializeComponent();
+            LiveCharts.Wpf.GeoMap geoMap = new LiveCharts.Wpf.GeoMap();
+            Random r = new Random();
+            Dictionary<string, double> map = new Dictionary<string, double>();
+            map["W"] = r.Next(0, 100);
+            map["OOE"] = r.Next(0, 100);
+            map["NOE"] = r.Next(0, 100);
+            map["BGL"] = r.Next(0, 100);
+            map["SBG"] = r.Next(0, 100);
+            map["KTN"] = r.Next(0, 100);
+            map["TIR"] = r.Next(0, 100);
+            map["VAB"] = r.Next(0, 100);
+            map["SMK"] = r.Next(0, 100);
+            geoMap.HeatMap = map;
+            geoMap.Source = @"Austria.xml";
+
+            DataContext = this;
+
+
+        }
+
+        private void d1bNewCancerRegistry_Click(object sender, RoutedEventArgs e)
+        {
+            new CancerRegistry1().Show();
+
+        }
+
+        private void erstellen_Click(object sender, RoutedEventArgs e)
+        {
+            new CancerRegistry1().Show();
+        }
+
+        private void Beenden(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
--- Krebsregister/MainWindow.xaml.cs
+++ Krebsregister/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LiveCharts.Wpf;
+using LiveCharts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +25,136 @@ namespace Krebsregister
         public MainWindow()
         {
             InitializeComponent();
+            AreaChart();
+            BarChart();
+            PieChart();
+            NegativStackChart();
+        }
+
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> YFormatter { get; set; }
+        private void AreaChart()
+        {
+            SeriesCollection = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title = "Series 1",
+                    Values = new ChartValues<double> { 4, 6, 5, 2 ,4 }
+                },
+                new LineSeries
+                {
+                    Title = "Series 2",
+                    Values = new ChartValues<double> { 6, 7, 3, 4 ,6 },
+                    PointGeometry = null
+                },
+                new LineSeries
+                {
+                    Title = "Series 3",
+                    Values = new ChartValues<double> { 4,2,7,2,7 },
+                    PointGeometry = DefaultGeometries.Square,
+                    PointGeometrySize = 15
+                }
+            };
+
+            Labels = new[] { "Jan", "Feb", "Mar", "Apr", "May" };
+            YFormatter = value => value.ToString("C");
+
+            //modifying the series collection will animate and update the chart
+            SeriesCollection.Add(new LineSeries
+            {
+                Title = "Series 4",
+                Values = new ChartValues<double> { 5, 3, 2, 4 },
+                LineSmoothness = 0, //0: straight lines, 1: really smooth lines
+                PointGeometry = Geometry.Parse("m 25 70.36218 20 -28 -20 22 -8 -6 z"),
+                PointGeometrySize = 50,
+                PointForeground = Brushes.Gray
+            });
+
+            //modifying any series values will also animate and update the chart
+            SeriesCollection[3].Values.Add(5d);
+
+            DataContext = this;
+        }
+        public SeriesCollection SeriesCollectionBC { get; set; }
+        public string[] LabelsBC { get; set; }
+        public Func<double, string> Formatter { get; set; }
+        private void BarChart()
+        {
+            SeriesCollectionBC = new SeriesCollection
+            {
+                new ColumnSeries
+                {
+                    Title = "2015",
+                    Values = new ChartValues<double> { 10, 50, 39, 50 }
+                }
+            };
+
+            //adding series will update and animate the chart automatically
+            SeriesCollectionBC.Add(new ColumnSeries
+            {
+                Title = "2016",
+                Values = new ChartValues<double> { 11, 56, 42 }
+            });
+
+            //also adding values updates and animates the chart automatically
+            SeriesCollectionBC[1].Values.Add(48d);
+
+            LabelsBC = new[] { "Maria", "Susan", "Charles", "Frida" };
+            Formatter = value => value.ToString("N");
+
+            DataContext = this;
+        }
+
+        public Func<ChartPoint, string> PointLabel { get; set; }
+        private void PieChart()
+        {
+            PointLabel = chartPoint =>
+                string.Format("{0} ({1:P})", chartPoint.Y, chartPoint.Participation);
+
+            DataContext = this;
+        }
+
+        private void Chart_OnDataClick(object sender, ChartPoint chartpoint)
+        {
+            var chart = (LiveCharts.Wpf.PieChart)chartpoint.ChartView;
+
+            //clear selected slice.
+            foreach (PieSeries series in chart.Series)
+                series.PushOut = 0;
+
+            var selectedSeries = (PieSeries)chartpoint.SeriesView;
+            selectedSeries.PushOut = 8;
+        }
+
+        public SeriesCollection SeriesCollectionNSC { get; set; }
+        public string[] LabelsNSC { get; set; }
+        public Func<double, string> FormatterNSC { get; set; }
+        private void NegativStackChart()
+        {
+            SeriesCollectionNSC = new SeriesCollection
+            {
+                new StackedRowSeries
+                {
+                    Title = "Male",
+                    Values = new ChartValues<double> {.5, .7, .8, .8, .6, .2, .6}
+                },
+                new StackedRowSeries
+                {
+                    Title = "Female",
+                    Values = new ChartValues<double> {-.5, -.7, -.8, -.8, -.6, -.2, -.6}
+                }
+            };
+
+            LabelsNSC = new[] { "0-20", "20-35", "35-45", "45-55", "55-65", "65-70", ">70" };
+            FormatterNSC = value => Math.Abs(value).ToString("P");
+
+            DataContext = this;
+        }
+        private void bDesign1_Click(object sender, RoutedEventArgs e)
+        {
+            new Design1().Show();
         }
     }
 }
