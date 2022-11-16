c4ea121367010ade636cba55887d4f079d7b73be
stadlbauera <StadlbauerA180120@sus.htl-grieskirchen.at>
2022-11-16 15:13:04 +0100

    Charts im Dashboard

--- Krebsregister/Dashboard.xaml.cs
+++ Krebsregister/Dashboard.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +24,28 @@ namespace Krebsregister
         public Dashboard()
         {
             InitializeComponent();
+            PieChart();
+        }
+
+        public Func<ChartPoint, string> PointLabel { get; set; }
+        public void PieChart()
+        {
+            PointLabel = chartPoint =>
+                string.Format("{0} ({1:P})", chartPoint.Y, chartPoint.Participation);
+
+            DataContext = this;
+        }
+
+        public void PieChart_DataClick(object sender, ChartPoint chartpoint)
+        {
+            var chart = (LiveCharts.Wpf.PieChart)chartpoint.ChartView;
+
+            //clear selected slice.
+            foreach (PieSeries series in chart.Series)
+                series.PushOut = 0;
+
+            var selectedSeries = (PieSeries)chartpoint.SeriesView;
+            selectedSeries.PushOut = 8;
         }
     }
 }
